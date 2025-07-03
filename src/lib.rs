266268f41466f9408ad2bb737d6c2d94f91e73a9
Rob Malouf <rmalouf@sdsu.edu>
2025-07-03 12:28:36 -0700

    Remove unused import

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,12 @@
 #![allow(clippy::unused_unit)]
 
 use polars::prelude::*;
-use pyo3::prelude::*;
 use pyo3_polars::PolarsAllocator;
 use pyo3_polars::derive::polars_expr;
 use whichlang::detect_language;
 
 #[global_allocator]
 static ALLOC: PolarsAllocator = PolarsAllocator::new();
-
-// #[pymodule]
-// fn _internal(_py: Python, _m: &Bound<PyModule>) -> PyResult<()> {
-//     Ok(())
-// }
 
 #[polars_expr(output_type=String)]
 fn detect_lang(inputs: &[Series]) -> PolarsResult<Series> {
